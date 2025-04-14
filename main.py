044cc7d9524158a0bd5b7dc59fc441be25aeca45
devme4f <devme4f@gmail.com>
2025-04-14 12:10:49 +0700

    update

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import argparse
 def checkJavaPath(java_path):
     try:
         cmd = subprocess.run(f"{java_path} -version", shell=True, capture_output=True, text=True)
-        print('[FOUND] - found java, recommend jdk17 or higher')
+        print('FOUND: found java, recommend jdk17 or higher')
     except subprocess.CalledProcessError:
-        print(f"[ERROR] - java not found, make sure is available on your PATH or specify it with '-jp' flag")
+        print(f"ERROR: java not found, make sure is available on your PATH or specify it with '-jp' flag")
         exit()
 
 def decompile(jav_file, out_jav_folder, JAVA_PATH):
@@ -17,7 +17,7 @@ def decompile(jav_file, out_jav_folder, JAVA_PATH):
     try:
         subprocess.run(command, check=True, shell=True)
     except subprocess.CalledProcessError as e:
-        print(f"[ERROR] - Error decompile: {jav_file}")
+        print(f"ERROR: Error decompile: {jav_file}")
         return
 
     os.chmod(jav_file, stat.S_IWRITE)
@@ -31,29 +31,29 @@ def getReady(jar_file):
     return jar_file, out_jar_folder
 
 def decompileJars(jar_files, JAVA_PATH):
-    print(f'[STATUS] - found {len(jar_files)} jar files')
+    print(f'STATUS: found {len(jar_files)} jar files')
     for i, jar_file in enumerate(jar_files):
         jar_file, out_jar_folder = getReady(jar_file)
         decompile(jar_file, out_jar_folder, JAVA_PATH)
 
         if i % 10 == 0 and i != 0:
-            print(f'[STATUS] - done {i} jars')
-    print(f'[STATUS] - done all jars')
+            print(f'STATUS: done {i} jars')
+    print(f'STATUS: done all jars')
 
 def decompileClasses(class_files, JAVA_PATH):
-    print(f'[STATUS] - found {len(class_files)} class files')
+    print(f'STATUS: found {len(class_files)} class files')
     for i, class_file in enumerate(class_files):
         class_file, out_class_folder = getReady(class_file)
         out_class_folder = os.path.dirname(out_class_folder)
         decompile(class_file, out_class_folder, JAVA_PATH)
 
         if i % 50 == 0 and i != 0:
-            print(f'[STATUS] - done {i} classes')
-    print(f'[STATUS] - done all classes')
+            print(f'STATUS: done {i} classes')
+    print(f'STATUS: done all classes')
 
 # beautify xml file, resource file in jar after decompile is minified
 def beautifyXML(xml_files):
-    print(f'[STATUS] - found {len(xml_files)} xml files')
+    print(f'STATUS: found {len(xml_files)} xml files')
     for xml_file in xml_files:
         xml_file, out_xml_folder = getReady(xml_file)
 
@@ -63,15 +63,16 @@ def beautifyXML(xml_files):
                 len_lines = len(text_lines)
 
             if len_lines < 3:
-                print(f'[STATUS] - beautifying: {xml_file}')
+                print(f'STATUS: beautifying: {xml_file}')
                 text = ''.join(text_lines)
                 parsed_xml = xml.dom.minidom.parseString(text)
                 new_xml = parsed_xml.toprettyxml(indent="\t")
                 with open(file=xml_file, mode='w', encoding='utf-8') as f:
                     f.write(new_xml)
         except Exception as e:
-            print(f'[ERROR] Error beautifying: {xml_file}')
+            print(f'ERROR: Error beautifying: {xml_file}')
             print(e)
+    print(f'STATUS: done beautify all xml files')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -82,10 +83,10 @@ if __name__ == '__main__':
     # conf
     PROJECT_FOLDER_PATH = (args.folder).strip()
     if PROJECT_FOLDER_PATH == None or PROJECT_FOLDER_PATH == '':
-        print('[ERORR] - folder location is required, please specify with -f flag')
+        print('ERROR: folder location is required, please specify with -f flag')
         exit()
     else:
-        print(f'[INFO] - Location: {PROJECT_FOLDER_PATH}')
+        print(f'INFO: Location: {PROJECT_FOLDER_PATH}')
 
     ALL_JARS_REGEX = '/**/*.jar'
     ALL_CLASSES_REGEX = '/**/*.class'
